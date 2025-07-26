a63f0b4d112e42563607fba0461d89af794116a2
Alexandros Nikolaos Ziogas <31545860+alexnick83@users.noreply.github.com>
2025-07-26 08:59:44 +0200

    Improved tests.

--- tests/qttools/utils/test_input_utils_dist.py
+++ tests/qttools/utils/test_input_utils_dist.py
@@ -2,10 +2,47 @@
 
 import numpy as np
 import pytest
+from mpi4py import MPI
 
 from qttools import xp
+from qttools.comm import comm
+from qttools.datastructures import DSDBCOO
 from qttools.utils.input_utils import create_hamiltonian
 from qttools.utils.mpi_utils import get_section_sizes
+
+
+# TODO test for block distributed
+@pytest.fixture(
+    autouse=True,
+    scope="module",
+)
+def configure_comm():
+    """Setup any state specific to the execution of the given module."""
+    block_comm_size = MPI.COMM_WORLD.Get_size()
+
+    # Default configuration setup based on the xp module
+    if xp.__name__ == "cupy":
+        _default_config = {
+            "all_to_all": "host_mpi",
+            "all_gather": "host_mpi",
+            "all_reduce": "host_mpi",
+            "bcast": "host_mpi",
+        }
+    elif xp.__name__ == "numpy":
+        _default_config = {
+            "all_to_all": "device_mpi",
+            "all_gather": "device_mpi",
+            "all_reduce": "device_mpi",
+            "bcast": "device_mpi",
+        }
+
+    # Configure the comm singleton with the parameterized block_comm_size
+    comm.configure(
+        block_comm_size=block_comm_size,
+        block_comm_config=_default_config,
+        stack_comm_config=_default_config,
+        override=True,
+    )
 
 
 @pytest.mark.parametrize(
@@ -83,11 +120,8 @@ def test_create_hamiltonian_dist(
 
     for rank in range(num_ranks):
 
-        start_idx = section_offsets[rank]
-        end_idx = section_offsets[rank + 1]
-
-        block_start = start_idx
-        block_end = end_idx
+        block_start = section_offsets[rank]
+        block_end = section_offsets[rank + 1]
 
         local_ham, _ = create_hamiltonian(
             hr,
@@ -116,3 +150,63 @@ def test_create_hamiltonian_dist(
             local_ham_dense = None
 
     assert xp.allclose(global_ham_dense, tmp)
+
+
+@pytest.mark.mpi
+@pytest.mark.parametrize("format", ["coo", "csr"])
+def test_create_hamiltonian_dsdbcoo(format: str):
+    hr = xp.ones((3, 3, 3, 5, 5))
+    num_transport_cells = 10
+    transport_dir = "x"
+    transport_cell = (2, 1, 1)
+    cutoff = 2
+    coords = xp.zeros((5, 3))
+    lat_vecs = xp.eye(3)
+
+    global_ham, bsizes = create_hamiltonian(
+        hr,
+        num_transport_cells,
+        transport_dir=transport_dir,
+        transport_cell=transport_cell,
+        block_start=0,
+        block_end=num_transport_cells,
+        return_sparse=True,
+        cutoff=cutoff,
+        coords=coords,
+        lattice_vectors=lat_vecs,
+        format=format,
+    )
+
+    global_ham_dense = global_ham.todense()
+    global_ham = None
+
+    num_ranks = comm.block.size
+    rank = comm.block.rank
+
+    section_sizes, __ = get_section_sizes(num_transport_cells, num_ranks)
+    section_offsets = np.hstack(([0], np.cumsum(section_sizes)))
+
+    block_start = section_offsets[rank]
+    block_end = section_offsets[rank + 1]
+
+    local_ham, _ = create_hamiltonian(
+        hr,
+        num_transport_cells,
+        transport_dir=transport_dir,
+        transport_cell=transport_cell,
+        block_start=block_start,
+        block_end=block_end,
+        return_sparse=True,
+        cutoff=cutoff,
+        coords=coords,
+        lattice_vectors=lat_vecs,
+        format=format,
+    )
+
+    dsdbcoo = DSDBCOO.from_sparray(
+        local_ham,
+        bsizes,
+        (1,),
+    )
+    dense = dsdbcoo.to_dense()
+    assert xp.allclose(global_ham_dense, dense)

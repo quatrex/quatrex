ce28151127f21bcd0597d3c3a4739421f873669d
vetschn <vetschnicolas@gmail.com>
2025-08-04 18:27:25 +0200

    First draft of wave function solver interface, implementations (cuDSS, LU, MUMPS) and corresponding tests.
    Everything now follows a predefined interface. MUMPS and cuDSS are able to reuse factorizations.

---
+++ src/qttools/wave_function_solver/solver.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2024 ETH Zurich and the authors of the qttools package.
+
+from abc import ABC, abstractmethod
+
+from qttools import NDArray, sparse
+
+
+class WFSolver(ABC):
+    """Abstract base class for wave function solvers."""
+
+    @abstractmethod
+    def solve(
+        self,
+        a: sparse.spmatrix,
+        b: NDArray,
+    ) -> NDArray:
+        """Solves the sparse linear system a @ x = b.
+
+        Parameters
+        ----------
+        a : sparse.spmatrix
+            The sparse system matrix.
+        b : NDArray
+            The right-hand side vector.
+
+        Returns
+        -------
+        x : NDArray
+            The solution vector.
+
+        """
---
+++ tests/qttools/wave_function_solver/test_solve.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2025 ETH Zurich and the authors of the qttools package.
+
+import importlib.util
+
+import numpy as np
+import pytest
+
+from qttools import NDArray, sparse, xp
+from qttools.wave_function_solver import LU, MUMPS, cuDSS
+
+mumps_available = importlib.util.find_spec("mumps") is not None
+nvmath_available = importlib.util.find_spec("nvmath") is not None
+
+
+def _assemble_system(
+    n: int,
+    m: int,
+    format: str,
+    order="C",
+) -> tuple[sparse.csc_matrix, NDArray]:
+    """Assembles a random sparse system of equations.
+
+    Parameters
+    ----------
+    n : int
+        Number of rows/columns in the square matrix.
+    m : int
+        Number of columns in the right-hand side matrix.
+    format : str
+        The format of the sparse matrix (e.g., 'csc', 'csr', 'coo').
+    order : str, optional
+        The memory order of the right-hand side matrix, 'C' for row-major or '
+        'F' for column-major. Default is 'C'.
+
+    Returns
+    -------
+    a : sparse.csc_matrix
+        The generated sparse system matrix in the specified format.
+    b : NDArray
+        The right-hand side matrix with shape (n, m).
+
+    """
+    a = sparse.random(n, n, density=0.1, format=format, dtype=float)
+    a += 1j * sparse.random(n, n, density=0.1, format=format, dtype=float)
+    a += sparse.diags([2.0] * n, format=format, dtype=float)
+    b = xp.ones((n, m), dtype=xp.complex128, order=order)
+    return a, b
+
+
+class TestLU:
+    """Tests for the LU wave function solver."""
+
+    def test_solve(self, n: int, m: int):
+        """Tests the wave function solver."""
+        a, b = _assemble_system(n, m, format="csc")
+        solver = LU()
+
+        x = solver.solve(a, b)
+
+        assert x.shape == (n, m)
+        assert xp.allclose(a @ x, b, atol=1e-6)
+
+
+@pytest.mark.skipif(not mumps_available, reason="Requires pytohn-mumps package")
+@pytest.mark.skipif(xp.__name__ != "numpy", reason="Requires numpy backend")
+class TestMUMPS:
+    def test_solve(self, n: int, m: int):
+        """Tests the wave function solver."""
+        a, b = _assemble_system(n, m, format="coo")
+
+        solver = MUMPS(reuse_analysis=False)
+        x = solver.solve(a, b)
+
+        assert x.shape == (n, m)
+        assert np.allclose(a @ x, b, atol=1e-6)
+
+    def test_reuse_analysis(self, n: int, m: int):
+        """Tests the wave function solver with reuse of analysis."""
+        a, b = _assemble_system(n, m, format="coo")
+
+        solver = MUMPS(reuse_analysis=True)
+        x1 = solver.solve(a, b)
+        assert np.allclose(a @ x1, b, atol=1e-6)
+
+        # Reuse the analysis phase.
+        a.data *= 10  # Modify the matrix to change the solution.
+
+        x2 = solver.solve(a, b)
+        assert np.allclose(a @ x2, b, atol=1e-6)
+
+    def test_explicit_ordering(self, n: int, m: int):
+        """Tests the wave function solver with explicit ordering."""
+        a, b = _assemble_system(n, m, format="coo")
+
+        solver = MUMPS(ordering="scotch")
+        x = solver.solve(a, b)
+
+        assert x.shape == (n, m)
+        assert np.allclose(a @ x, b, atol=1e-6)
+
+
+@pytest.mark.skipif(not nvmath_available, reason="Requires nvmath-python package")
+@pytest.mark.skipif(xp.__name__ != "cupy", reason="Requires cupy backend")
+class TestcuDSS:
+
+    def test_solve(self, n: int, m: int):
+        """Tests the wave function solver."""
+        a, b = _assemble_system(n, m, format="csr", order="F")
+
+        solver = cuDSS()
+        x = solver.solve(a, b)
+
+        assert x.shape == (n, m)
+        assert xp.allclose(a @ x, b, atol=1e-6)
+
+    def test_explicit_reset_operands(self, n: int, m: int):
+        """Tests the wave function solver with explicit reset of operands."""
+        a, b = _assemble_system(n, m, format="csr", order="F")
+
+        solver = cuDSS(explicitely_reset_operands=True)
+        x1 = solver.solve(a, b)
+        assert xp.allclose(a @ x1, b, atol=1e-6)
+
+        # Modify the matrix to change the solution.
+        a.data *= 10
+
+        x2 = solver.solve(a, b)
+        assert xp.allclose(a @ x2, b, atol=1e-6)
+
+    def test_implicit_reset_operands(self, n: int, m: int):
+        """Tests the wave function solver without explicit reset of operands."""
+        a, b = _assemble_system(n, m, format="csr", order="F")
+
+        solver = cuDSS(explicitely_reset_operands=False)
+        x1 = solver.solve(a, b)
+        assert xp.allclose(a @ x1, b, atol=1e-6)
+
+        # Modify the matrix to change the solution.
+        a.data *= 10
+
+        # The solver should still work without explicit reset.
+        x2 = solver.solve(a, b)
+        assert xp.allclose(a @ x2, b, atol=1e-6)

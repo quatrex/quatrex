c8eb3df6c4f49af96964f927bd23c0ab017d154a
mdossena <mdossena@iis.ee.ethz.ch>
2025-09-04 10:23:13 +0200

    Added return into _eig_cupy and added coloumns removal in full

--- src/qttools/nevp/full.py
+++ src/qttools/nevp/full.py
@@ -35,7 +35,7 @@ class Full(NEVP):
 
     def __init__(
         self,
-        eig_compute_location: str = "numpy",
+        eig_compute_location: str = "cupy",
         use_pinned_memory: bool = True,
     ):
         """Initializes the Full NEVP solver."""
@@ -76,11 +76,43 @@ class Full(NEVP):
         B = xp.kron(xp.tri(len(a_xx) - 2).T, xp.eye(a_xx[0].shape[-1]))
         A[:, : B.shape[0], : B.shape[1]] -= B
 
+        print("FULL: Size of matrix before deleting elements:", A.shape)
+
+        # Find coloumns with all zeros
+        nnz = xp.count_nonzero(A[0], axis=0)
+        diag = xp.diagonal(A[0])
+
+        i_z = xp.where(nnz == 0)[0]
+
+        # Find coloumns with -1 on the diag and with norm 1
+        i_mo = xp.where((diag == -1) & (nnz == 1))[0]
+
+        # Concatenate and delete
+        i_B = xp.concatenate((i_z, i_mo))
+        i_A = xp.setdiff1d(xp.arange(A.shape[-1]), i_B)
+        i_tot = xp.concatenate((i_A, i_B))
+
+        A_b = A[:, i_B, :][:, :, i_B].diagonal(axis1=-2, axis2=-1)
+        A_c = A[:, i_B, :][:, :, i_A]
+        A = A[:, i_A, :][:, :, i_A]
+
+        print("FULL: Size of matrix after deleting elements:", A.shape)
+
         w, v = linalg.eig(
             A,
             compute_module=self.eig_compute_location,
             use_pinned_memory=self.use_pinned_memory,
         )
+
+        v_y = xp.divide(
+            A_c @ v, w[:, xp.newaxis, :] - A_b[:, :, xp.newaxis]
+        )  # shape: (batch, len(i_B), num_eig)
+
+        # v[:,i_tot,:] = xp.concatenate([v, v_y], axis=1) Without Memory copy it does not work
+
+        v_pre_sort = xp.concatenate([v, v_y], axis=1)
+        v = xp.empty_like(v_pre_sort)
+        v[:, i_tot, :] = v_pre_sort
 
         # Recover the original eigenvalues from the spectral transform.
         w = xp.where((xp.abs(w) == 0.0), -1.0, w)

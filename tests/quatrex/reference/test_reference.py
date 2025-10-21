bbf9696801c0fd729984c5811c097c2bbd16f166
vetschn <vetschnicolas@gmail.com>
2025-10-21 07:47:47 +0200

    Fixed inconsistent naming.

--- tests/quatrex/reference/test_reference.py
+++ tests/quatrex/reference/test_reference.py
@@ -7,7 +7,7 @@ from importlib.resources import files
 import numpy as np
 import pytest
 
-from quatrex.cli.main import fetch_example, _run_scba
+from quatrex.cli.main import fetch_example, run
 from quatrex.examples import get_example_dir, load
 
 REFERENCE_OBSERVABLES = {
@@ -58,7 +58,7 @@ def test_non_distributed(non_distributed_example: str):
     if not compute_config_path.exists():
         compute_config_path = None
 
-    _run_scba(
+    run(
         quatrex_config_path,
         compute_config_path,
     )

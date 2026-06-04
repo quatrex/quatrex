2169c059a38d770b7294179c24909ec766644078
mdossena <mdossena@iis.ee.ethz.ch>
2026-06-04 23:26:13 +0200

    Cleanup

--- src/qttools/utils/hdf5_utils.py
+++ src/qttools/utils/hdf5_utils.py
@@ -9,6 +9,9 @@ from scipy import sparse
 
 def load_hdf5_dict(filename: str) -> dict:
     """Loads the given HDF5 file and returns its contents as a dictionary.
+    The dictionary values can be either numpy arrays or scipy sparse matrices in CSR,
+    COO, or CSC format. The dictionary keys should be strings in the format "[x,y,z]",
+    where x, y, and z are the coordinates of the corresponding matrix in the hopping grid.
 
     Parameters
     ----------
@@ -27,49 +30,43 @@ def load_hdf5_dict(filename: str) -> dict:
 
         for key in f.keys():
 
+            # Validate the key format (should be a string in the format "[x,y,z]"")
             if not re.fullmatch(r"\[[^,\[\]]+,[^,\[\]]+,[^,\[\]]+\]", key):
-                raise ValueError(f"Key '{key}' must be in the format [x,y,z].")
+                raise ValueError(f"Key '{key}' must be in the format '[x,y,z]'.")
 
+            # Load the item and determine its format
             item = f[key]
             fmt = item.attrs.get("format", None)
 
             if fmt == "ndarray":
                 matrix_dict[key] = item[:]
 
-            elif fmt == "csr":
+            elif fmt == "csr" or fmt == "csc" or fmt == "coo":
+
+                # If the format is a sparse matrix, we load the shape attribue
                 shape = item.attrs.get("shape", None)
+
                 if shape is None:
                     raise ValueError(
-                        f"Missing 'shape' attribute for CSR matrix '{key}' in HDF5 file."
+                        f"Missing 'shape' attribute for {fmt} matrix '{key}' in HDF5 file."
                     )
 
-                matrix_dict[key] = sparse.csr_matrix(
-                    (item["data"][:], item["indices"][:], item["indptr"][:]),
-                    shape=shape,
-                )
-
-            elif fmt == "coo":
-                shape = item.attrs.get("shape", None)
-                if shape is None:
-                    raise ValueError(
-                        f"Missing 'shape' attribute for COO matrix '{key}' in HDF5 file."
+                # Depending on the format, the correct sparse matrix is constructed
+                # from the corresponding datasets
+                if fmt == "csr":
+                    matrix_dict[key] = sparse.csr_matrix(
+                        (item["data"][:], item["indices"][:], item["indptr"][:]),
+                        shape=shape,
                     )
-
-                matrix_dict[key] = sparse.coo_matrix(
-                    (item["data"][:], (item["row"][:], item["col"][:])), shape=shape
-                )
-
-            elif fmt == "csc":
-                shape = item.attrs.get("shape", None)
-                if shape is None:
-                    raise ValueError(
-                        f"Missing 'shape' attribute for CSC matrix '{key}' in HDF5 file."
+                elif fmt == "coo":
+                    matrix_dict[key] = sparse.coo_matrix(
+                        (item["data"][:], (item["row"][:], item["col"][:])), shape=shape
                     )
-
-                matrix_dict[key] = sparse.csc_matrix(
-                    (item["data"][:], item["indices"][:], item["indptr"][:]),
-                    shape=shape,
-                )
+                elif fmt == "csc":
+                    matrix_dict[key] = sparse.csc_matrix(
+                        (item["data"][:], item["indices"][:], item["indptr"][:]),
+                        shape=shape,
+                    )
 
             else:
                 raise ValueError(
@@ -81,6 +78,9 @@ def load_hdf5_dict(filename: str) -> dict:
 
 def save_hdf5_dict(filename: str, data: dict):
     """Saves a dictionary to an HDF5 file.
+    The dictionary values can be either numpy arrays or scipy sparse matrices in CSR, COO, or CSC format.
+    The dictionary keys should be strings in the format "[x,y,z]", where x, y, and z are the coordinates
+    of the corresponding matrix in the hopping grid.
 
     Parameters
     ----------
@@ -95,8 +95,9 @@ def save_hdf5_dict(filename: str, data: dict):
 
     """
 
-    # VALIDATE IF THE DICTIONARY IS CONSISTENT WITH QUATREX FORMAT
+    # Validate if the keys and values in the dictionary are of the correct type and format
     for key, mat in data.items():
+
         if not isinstance(key, str):
             raise TypeError(
                 f"Keys in the dictionary must be strings, got {type(key)} for key '{key}'."
@@ -114,43 +115,38 @@ def save_hdf5_dict(filename: str, data: dict):
                 f"scipy.sparse.csc_matrix, and numpy.ndarray."
             )
 
-    # SAVE THE DICTIONARY TO THE HDF5 FILE
+    # Save the dictionary to the HDF5 file, storing the format and shape information as attributes
     with h5py.File(filename, "w") as f:
         for key, mat in data.items():
 
-            if isinstance(mat, sparse.csr_matrix):
+            if isinstance(
+                mat, (sparse.csr_matrix, sparse.csc_matrix, sparse.coo_matrix)
+            ):
+                # If the matrix is a sparse matrix, a group is created and the format and shape informations
+                # are stored as attributes
                 grp = f.create_group(key)
-                fmt = mat.format
-
-                grp.attrs["format"] = fmt
+                grp.attrs["format"] = mat.format
                 grp.attrs["shape"] = mat.shape
 
-                grp.create_dataset("data", data=mat.data, compression="gzip")
-                grp.create_dataset("indices", data=mat.indices, compression="gzip")
-                grp.create_dataset("indptr", data=mat.indptr, compression="gzip")
-
-            elif isinstance(mat, sparse.coo_matrix):
-                grp = f.create_group(key)
-                fmt = mat.format
-
-                grp.attrs["format"] = fmt
-                grp.attrs["shape"] = mat.shape
-
-                grp.create_dataset("data", data=mat.data, compression="gzip")
-                grp.create_dataset("row", data=mat.row, compression="gzip")
-                grp.create_dataset("col", data=mat.col, compression="gzip")
-
-            elif isinstance(mat, sparse.csc_matrix):
-                grp = f.create_group(key)
-                fmt = mat.format
-
-                grp.attrs["format"] = fmt
-                grp.attrs["shape"] = mat.shape
-
-                grp.create_dataset("data", data=mat.data, compression="gzip")
-                grp.create_dataset("indices", data=mat.indices, compression="gzip")
-                grp.create_dataset("indptr", data=mat.indptr, compression="gzip")
+                # Depending on the format, the correct datasets are created for the sparse matrix data
+                if isinstance(mat, (sparse.csr_matrix, sparse.csc_matrix)):
+                    grp.create_dataset("data", data=mat.data, compression="gzip")
+                    grp.create_dataset("indices", data=mat.indices, compression="gzip")
+                    grp.create_dataset("indptr", data=mat.indptr, compression="gzip")
+                else:
+                    grp.create_dataset("data", data=mat.data, compression="gzip")
+                    grp.create_dataset("row", data=mat.row, compression="gzip")
+                    grp.create_dataset("col", data=mat.col, compression="gzip")
 
             elif isinstance(mat, np.ndarray):
+                # If the matrix is a dense numpy array, it is directly stored as a
+                # dataset with the format information as an attribute
                 dset = f.create_dataset(key, data=mat, compression="gzip")
                 dset.attrs["format"] = "ndarray"
+
+            else:
+                raise TypeError(
+                    f"Unsupported data type {type(mat)} for key '{key}'. "
+                    f"Supported types are: scipy.sparse.csr_matrix, scipy.sparse.coo_matrix, "
+                    f"scipy.sparse.csc_matrix, and numpy.ndarray."
+                )

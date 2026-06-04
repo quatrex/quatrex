e3dfbd25a51f43a1f4cc88179ede84f5fee90387
mdossena <mdossena@iis.ee.ethz.ch>
2026-06-04 11:21:37 +0200

    Fixed bug with test

--- tests/qttools/utils/test_hdf5_utils.py
+++ tests/qttools/utils/test_hdf5_utils.py
@@ -2,9 +2,9 @@
 
 from pathlib import Path
 
+import numpy as np
 import scipy.sparse as sps
 
-from qttools import xp
 from qttools.utils.hdf5_utils import load_hdf5_dict, save_hdf5_dict
 
 
@@ -12,7 +12,7 @@ def test_save_load_h5(tmp_path: Path):
     """Test the hdf5 save and load functions."""
 
     dict = {
-        "[0,0,0]": xp.random.rand(10, 10),
+        "[0,0,0]": np.random.rand(10, 10),
         "[1,0,0]": sps.random(10, 10, density=0.5, format="csr"),
         "[0,1,0]": sps.random(10, 10, density=0.5, format="coo"),
         "[0,0,1]": sps.random(10, 10, density=0.5, format="csc"),
@@ -23,6 +23,6 @@ def test_save_load_h5(tmp_path: Path):
 
     for key in dict.keys():
         if isinstance(dict[key], sps.spmatrix):
-            assert xp.allclose(dict[key].toarray(), loaded_dict[key].toarray())
+            assert np.allclose(dict[key].toarray(), loaded_dict[key].toarray())
         else:
-            assert xp.allclose(dict[key], loaded_dict[key])
+            assert np.allclose(dict[key], loaded_dict[key])
--- tests/qttools/utils/test_mpi_utils.py
+++ tests/qttools/utils/test_mpi_utils.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 
+import numpy as np
 import pytest
 import scipy.sparse as sps
 from mpi4py import MPI
@@ -107,13 +108,17 @@ def test_distributed_load_npz(mpi_tmp_path: Path):
     assert xp.allclose(coo.toarray(), loaded_arr.toarray())
 
 
+@pytest.mark.skipif(
+    _is_multi_node(),
+    reason="This test only works if all ranks see the same file system.",
+)
 @pytest.mark.mpi(min_size=2)
 def test_distributed_load_h5(mpi_tmp_path: Path):
     """Test the distributed_load function."""
     dict = None
     if global_comm.rank == 0:
         dict = {
-            "[0,0,0]": xp.random.rand(10, 10),
+            "[0,0,0]": np.random.rand(10, 10),
             "[1,0,0]": sps.random(10, 10, density=0.5, format="csr"),
             "[0,1,0]": sps.random(10, 10, density=0.5, format="coo"),
             "[0,0,1]": sps.random(10, 10, density=0.5, format="csc"),
@@ -122,13 +127,20 @@ def test_distributed_load_h5(mpi_tmp_path: Path):
 
     dict = global_comm.bcast(dict, root=0)
 
+    # Distributed_load converts the keys back to tuples of ints, so we need to do the same for the original dict.
+    dict = {
+        tuple(map(int, r.strip("[]").split(","))): h_r
+        for r, h_r in dict.items()
+        if r.startswith("[")
+    }
+
     loaded_dict = distributed_load(mpi_tmp_path / "dict.h5")
 
     for key in dict.keys():
         if isinstance(dict[key], sps.spmatrix):
-            assert xp.allclose(dict[key].toarray(), loaded_dict[key].toarray())
+            assert np.allclose(dict[key].toarray(), loaded_dict[key].toarray())
         else:
-            assert xp.allclose(dict[key], loaded_dict[key])
+            assert np.allclose(dict[key], loaded_dict[key])
 
 
 @pytest.mark.mpi(min_size=2)
